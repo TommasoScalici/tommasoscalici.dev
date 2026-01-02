d0c4c9d0e57c869474c587409f3f64ba4d6bec48
Tommaso Scalici <tommaso.scalici.1991@gmail.com>
2026-01-02 18:25:46 +0100

    feat: Add debug logging and remove prerender from index page, and configure Vite SSR external dependencies.

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,4 +19,10 @@ export default defineConfig({
         domains: ['f4.bcbits.com', 'googleusercontent.com', 'i.scdn.co'],
         remotePatterns: [{ protocol: 'https' }],
     },
+    vite: {
+        ssr: {
+            external: ['node:fs', 'node:path'],
+        },
+    },
+
 });
